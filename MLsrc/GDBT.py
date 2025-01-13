771d8f1fbc61f3bc9e783b1bf183090490d88a60
cheungbh <cheungbh@cheungbhdeMacBook-Pro.local>
2025-01-13 22:19:02 +0800

    add GBDT save file

--- MLsrc/GDBT.py
+++ MLsrc/GDBT.py
@@ -3,6 +3,7 @@ import csv
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from joblib import dump
 import argparse
+import os
 
 
 def load_data(file_path, feature_num):
@@ -10,7 +11,7 @@ def load_data(file_path, feature_num):
     data_target = []
     csv_file = csv.reader(open(file_path))
     for content in csv_file:
-        content = list(map(float, content[:-2]))
+        # content = list(map(float, content[:data_final]))
         if len(content) != 0:
             data_feature.append(content[0:feature_num])
             data_target.append(content[feature_num])
@@ -19,17 +20,23 @@ def load_data(file_path, feature_num):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_file", type=str, help="Path to the training CSV file")
-    parser.add_argument("--test_file", type=str, help="Path to the testing CSV file")
+    # parser.add_argument("--train_file", type=str, help="Path to the training CSV file")
+    # parser.add_argument("--test_file", type=str, help="Path to the testing CSV file")
+    parser.add_argument("--data_root", type=str, help="Path to the data root")
     parser.add_argument("--output_folder", type=str, help="Path to save the trained model")
-    parser.add_argument("--csv_path", type=str, help="Path to save the results in csv")
+    parser.add_argument("--save_lowest_acc", type=float, default=90)
+    # parser.add_argument("--csv_path", type=str, help="Path to save the results in csv")
     args = parser.parse_args()
+    os.makedirs(args.output_folder, exist_ok=True)
 
     save_model = False
+    train_file = f"{args.data_root}/train.csv"
+    test_file = f"{args.data_root}/val.csv"
+    csv_path = f"{args.output_folder}/results.csv"
 
-    feature_num = 26
-    train_feature, train_target = load_data(args.train_file, feature_num)
-    test_feature, test_target = load_data(args.test_file, feature_num)
+    feature_num = 14
+    train_feature, train_target = load_data(train_file, feature_num)
+    test_feature, test_target = load_data(test_file, feature_num)
 
     if save_model is True:
         gbdt = GradientBoostingClassifier()
@@ -59,15 +66,15 @@ def main():
         print("\n\nOverall result:")
         print(classification_report(test_target, predict_results_test))
 
-        csv_file_path = args.csv_path
+        # csv_file_path = args.csv_path
 
         result_title = ["algo", 'Train Acc', 'Test Acc'] + ['Class ' + str(i) + ' Acc' for i in range(len(cls_result))]
         result_data = ["GBDT", train_accuracy, test_accuracy] + [acc for acc in cls_result]
 
-        with open(csv_file_path, 'a', newline='') as file:
+        with open(csv_path, 'a', newline='') as file:
             writer = csv.writer(file)
             empty = True
-            with open(csv_file_path, 'r') as file:
+            with open(csv_path, 'r') as file:
                 csv_reader = csv.reader(file)
                 for row in csv_reader:
                     if row:
@@ -78,6 +85,7 @@ def main():
             else:
                 writer.writerow(result_data)
     else:
+        search_idx = 0
         for lr in [0.1, 0.2, 0.3]:
             for est in [100, 200, 300]:
                 for sub in [0.5, 1.0]:
@@ -86,15 +94,24 @@ def main():
                             for weight_fraction_leaf in [0, 0.1, 0.3]:
                                 for depth in [1, 3, 10]:
                                     for impurity_decrease in [0, 0.5, 1]:
-                                        gbdt = GradientBoostingClassifier(learning_rate=lr, n_estimators=est, subsample=sub, min_samples_split=sample_split, min_samples_leaf=sample_leaf, min_weight_fraction_leaf=weight_fraction_leaf, max_depth=depth, min_impurity_decrease=impurity_decrease)
+                                        search_idx += 1
+                                        gbdt = GradientBoostingClassifier(learning_rate=lr,
+                                                                          n_estimators=est,
+                                                                          subsample=sub,
+                                                                          min_samples_split=sample_split,
+                                                                          min_samples_leaf=sample_leaf,
+                                                                          min_weight_fraction_leaf=weight_fraction_leaf,
+                                                                          max_depth=depth,
+                                                                          min_impurity_decrease=impurity_decrease)
                                         gbdt.fit(train_feature, train_target)
                                         predict_results_train = gbdt.predict(train_feature)
                                         predict_results_test = gbdt.predict(test_feature)
-
                                         train_accuracy = accuracy_score(predict_results_train, train_target)
                                         # print("\n\nGBDT " + f"Learning_rate: {lr}, n_estimators: {est}, subsample: {sub}, min_samples_split: {sample_split}, min_samples_leaf: {sample_leaf}, min_weight_fraction_leaf: {weight_fraction_leaf}, max_depth: {depth}, min_impurity_decrease: {impurity_decrease}")
                                         # print("Training accuracy is : ", train_accuracy)
                                         test_accuracy = accuracy_score(predict_results_test, test_target)
+                                        if (test_accuracy * 100) > args.save_lowest_acc:
+                                            dump(gbdt, f"{args.output_folder}/model_{search_idx}_{test_accuracy}.joblib")
                                         # print("Testing accuracy is : ", test_accuracy)
                                         # print("The test confusion matrix is:")
                                         conf_mat = confusion_matrix(test_target, predict_results_test)
@@ -111,15 +128,15 @@ def main():
                                         print(cls_result)
                                         print("\n\nOverall result:")
                                         print(classification_report(test_target, predict_results_test))
-                                        csv_file_path = args.csv_path
+                                        # csv_file_path = args.csv_path
 
                                         result_title = ["algo", 'Train Acc', 'Test Acc'] + ['Class ' + str(i) + ' Acc' for i in range(len(cls_result))]
                                         result_data = ["GBDT", train_accuracy, test_accuracy] + [acc for acc in cls_result] + [f"Learning_rate: {lr}, n_estimators: {est}, subsample: {sub}, min_samples_split: {sample_split}, min_samples_leaf: {sample_leaf}, min_weight_fraction_leaf: {weight_fraction_leaf}, max_depth: {depth}, min_impurity_decrease: {impurity_decrease}"]
 
-                                        with open(csv_file_path, 'a', newline='') as file:
+                                        with open(csv_path, 'a', newline='') as file:
                                             writer = csv.writer(file)
                                             empty = True
-                                            with open(csv_file_path, 'r') as file:
+                                            with open(csv_path, 'r') as file:
                                                 csv_reader = csv.reader(file)
                                                 for row in csv_reader:
                                                     if row:
@@ -129,7 +146,6 @@ def main():
                                                 writer.writerow(result_data)
                                             else:
                                                 writer.writerow(result_data)
-
 
 
 if __name__ == "__main__":
